bcb53bf5446a76b438926a220a9d386168271c31
TheOnlySmartBoy <o2jose43@gmail.com>
2018-01-03 19:33:23 +0300

    Added encryption settings for form

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+define('CURRENCY', '$');
+define('WEB_URL', 'http://127.0.0.1/ams/');
+define('ROOT_PATH', 'C:/wamp64/www/ams/');
+
+
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'Tosby');
+define('DB_PASSWORD', 'MasterTosby2');
+define('DB_DATABASE', 'ams_mb');
+$link = mysqli_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD);
+if(!$link){
+    die(mysqli_error($link));
+}
+ else {
+    $db= mysqli_select_db($link,DB_DATABASE);
+    if(!$db){
+    die(mysqli_error($link));
+    }
+    
+}
