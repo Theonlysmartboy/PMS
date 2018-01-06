2203f5e5b20b256652c6f0461ccbc6b1b57782bb
TheOnlySmartBoy <o2jose43@gmail.com>
2018-01-06 10:20:52 +0300

    Edited the cofiguration file

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('CURRENCY', '$');
+define('CURRENCY', 'Ksh');
 define('WEB_URL', 'http://127.0.0.1/Pms/');
 define('ROOT_PATH', 'C:/wamp64/www/Pms/');
 
@@ -7,15 +7,15 @@ define('ROOT_PATH', 'C:/wamp64/www/Pms/');
 define('DB_HOSTNAME', '127.0.0.1');
 define('DB_USERNAME', 'Tosby');
 define('DB_PASSWORD', 'MasterTosby2');
-define('DB_DATABASE', 'ams_mb');
+define('DB_DATABASE', 'pms_db');
 $link = mysqli_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD);
 if(!$link){
-    die(mysqli_error($link));
+    die("<script>alert('Could not connect to mysql server please contact admin for assistance');</script>");
 }
  else {
     $db= mysqli_select_db($link,DB_DATABASE);
     if(!$db){
-    die(mysqli_error($link));
+    die("<script> alert('Could not load database please contact admin for assistance');</script>");
     }
     
 }
