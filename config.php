b9487e5f5c52859e4042a90266ed05ffd4c95893
TheOnlySmartBoy <o2jose43@gmail.com>
2019-08-17 16:04:44 +0300

    Completed splashscreen controller code

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 define('CURRENCY', 'Ksh');
-define('ROOT_PATH', '/var/www/html/pms/');
-define('WEB_URL', 'http://192.168.1.210/pms/');
+define('ROOT_PATH', '/wamp/www/pms/');
+define('WEB_URL', 'http://localhost:8082/pms/');
 
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'vidic_pms');
-define('DB_PASSWORD', 'sql@VIDIC%18');
-define('DB_DATABASE', 'vidic_pms');
+define('DB_USERNAME', 'Tosby');
+define('DB_PASSWORD', 'MasterTosby2');
+define('DB_DATABASE', 'pms_db');
 $link = mysqli_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD) or die(mysql_error());
 mysqli_select_db($link,DB_DATABASE) or die(mysql_error());?>
